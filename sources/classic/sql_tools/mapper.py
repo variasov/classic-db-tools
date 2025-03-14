9cde9f8744a9983881099a49ebecbf2a3980c5dd
Sergey Variasov <variasov@gmail.com>
2025-03-14 14:51:28 +0300

    Работоспособные наброски маппера на строках

--- sources/classic/sql_tools/mapper.py
+++ sources/classic/sql_tools/mapper.py
@@ -3,12 +3,15 @@ from ast import *
 from typing import Sequence
 
 
+COL_OFFSET = ' ' * 4
+
+
 class Mapper(ABC):
     name: str
     identity_map_name: str
 
     @abstractmethod
-    def to_ast(self):
+    def to_sources(self, offsets: int):
         pass
 
 
@@ -25,26 +28,16 @@ class ToDict(Mapper):
         self.id = id
         self.cols = cols
 
-    def to_ast(self):
+    def to_sources(self, offsets: int):
         return [
-            Assign(
-                targets=[Name(id=self.name, ctx=Store())],
-                value=Call(
-                    func=Attribute(
-                        value=Name(id=self.identity_map_name, ctx=Load()),
-                        attr='get',
-                        ctx=Load(),
-                    ),
-                    args=[
-                        Subscript(
-                            value=Name(id='row', ctx=Load()),
-                            slice=Constant(value=self.id),
-                            ctx=Load(),
-                        )
-                    ],
-                    keywords=[],
-                )
+            COL_OFFSET * offsets + f"{self.name} = {self.identity_map_name}.get(row['{self.id}'])",
+            COL_OFFSET * offsets + f"if {self.name} is None:",
+            COL_OFFSET * (offsets + 1) + f"{self.name} = {self.identity_map_name}[row['{self.id}']] = {{",
+            *(
+                COL_OFFSET * (offsets + 2) + f"'{key}': row['{col}'],"
+                for key, col in self.cols.items()
             ),
+            COL_OFFSET * (offsets + 1) + '}',
         ]
 
 
@@ -60,72 +53,32 @@ class OneToMany:
         self.attr = attr
         self.right = right
 
-    def to_ast(self):
-        return Expr(
-            value=Call(
-                func=Attribute(
-                    value=Subscript(
-                        value=Name(id=self.left, ctx=Load()),
-                        slice=Constant(value=self.attr),
-                        ctx=Load(),
-                    ),
-                    attr='append',
-                    ctx=Load()
-                ),
-                args=[Name(id=self.right, ctx=Load())],
-                keywords=[],
-            )
-        )
+    def to_sources(self, offsets: int):
+        return [
+            COL_OFFSET * offsets + f"if '{self.attr}' not in {self.left}:",
+            COL_OFFSET * (offsets + 1) + f"{self.left}['{self.attr}'] = []",
+            COL_OFFSET * offsets + f"{self.left}['{self.attr}'].append({self.right})",
+        ]
 
 
 def returning(*mappers, returns: str):
-    identity_maps = []
+    func_body = [f'def mapper_func(rows):']
     for obj in mappers:
         if isinstance(obj, Mapper):
-            identity_maps.append(
-                Assign(
-                    targets=[Name(id=obj.identity_map_name, ctx=Store())],
-                    value=Dict(keys=[], values=[]),
-                )
+            func_body.append(
+                f'{COL_OFFSET}{obj.identity_map_name} = {{}}'
             )
 
-    cycle_body = []
+    func_body.append(f'{COL_OFFSET}for row in rows:')
+
     for obj in mappers:
-        cycle_body.append(obj.to_ast())
+        func_body.extend(obj.to_sources(offsets=2))
 
-    cycle = For(
-        target=Name(id='row', ctx=Store()),
-        iter=Name(id='rows', ctx=Load()),
-        body=cycle_body,
-        orelse=[],
-    )
+    func_body.append(f'{COL_OFFSET}return list({returns}s.values())')
 
-    return_result = Return(value=Name(id=returns, ctx=Load()))
+    func_source = '\n'.join(func_body)
 
-    func = FunctionDef(
-        name='mapper_func',
-        args=arguments(
-            posonlyargs=[], args=[arg(arg='rows')], kwonlyargs=[],
-            kw_defaults=[], defaults=[],
-        ),
-        body=[
-            *identity_maps,
-            cycle,
-            return_result,
-        ],
-        decorator_list=[],
-        lineno=1,
-        col_offset=0,
-    )
-
-    # src = unparse(fix_missing_locations(func))
-
-    module = Module(body=[fix_missing_locations(func)], type_ignores=[])
-
-    code = compile(
-        module, '<string>', 'exec',
-        # src, '<string>', 'exec',
-    )
+    code = compile(func_source, '<string>', 'exec')
     namespace = {}
     exec(code, namespace)
     return namespace['mapper_func']
--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -30,35 +30,16 @@ result = [{
 
 def test_returning():
     func = returning(
-        ToDict('cakes', 'id', {
+        ToDict('cake', 'id', {
             'id': 'id',
             'name': 'name',
             'slices': 'slices',
         }),
-        ToDict('dudes', 'dude_id', {
+        ToDict('dude', 'dude_id', {
             'id': 'dude_id',
             'name': 'dude_name',
         }),
         OneToMany('cake', 'eaten_by', 'dude'),
-        returns='cakes'
+        returns='cake'
     )
     assert func(data) == result
-
-from ast import *
-
-# def test_tmp():
-#
-#     func = FunctionDef(
-#         name='mapper_func',
-#         args=arguments(
-#             posonlyargs=[], args=[arg(arg='rows')], kwonlyargs=[],
-#             kw_defaults=[], defaults=[],
-#         ),
-#         body=[
-#             Return(value=Constant(value=1)),
-#         ],
-#         decorator_list=[],
-#         lineno=1,
-#         col_offset=0,
-#     )
-
