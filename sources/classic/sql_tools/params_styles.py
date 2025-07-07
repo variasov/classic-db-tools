212e7e7e8d6d84d686ba85f05abec0c0f1b23350
Sergey Variasov <variasov@gmail.com>
2025-07-07 11:27:42 +0700

    Добавлена обработка случая с ScopedConnection

--- sources/classic/sql_tools/params_styles.py
+++ sources/classic/sql_tools/params_styles.py
@@ -1,5 +1,10 @@
 import sys
 
+try:
+    from classic.db_utils import ScopedConnection
+except ImportError:
+    ScopedConnection = None
+
 
 class ParamStyleRecognizer:
     known_styles: dict[type, str]
@@ -8,14 +13,19 @@ class ParamStyleRecognizer:
         self.known_styles = {}
 
     def get(self, conn: object) -> str:
+        if ScopedConnection is not None and isinstance(conn, ScopedConnection):
+            conn_cls = conn.wrapped_conn_cls
+        else:
+            conn_cls = conn.__class__
+
         try:
-            return self.known_styles[conn.__class__]
+            return self.known_styles[conn_cls]
         except KeyError:
-            modname = conn.__module__
+            modname = conn_cls.__module__
             while modname:
                 try:
                     style = sys.modules[modname].paramstyle  # type: ignore
-                    self.known_styles[conn.__class__] = style
+                    self.known_styles[conn_cls] = style
                     return style
                 except AttributeError:
                     if "." in modname:
