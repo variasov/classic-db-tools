81ecf55f5c893fbbb7975016904a1a254081dd02
Dmitry Pletnev <Dmitry.Pletnev@evraz.com>
2026-07-31 17:14:20 +0300

    добавлена поддержка psycopg2

--- sources/classic/db_tools/backends/psycopg2.py
+++ sources/classic/db_tools/backends/psycopg2.py
@@ -3,27 +3,58 @@ import psycopg2
 from ..conn_validator import ConnectionValidator
 from ..transaction import Transaction
 
-
 TRANSACTION_STATUS_UNKNOWN = psycopg2.extensions.TRANSACTION_STATUS_UNKNOWN
 TRANSACTION_STATUS_IDLE = psycopg2.extensions.TRANSACTION_STATUS_IDLE
 
 
 class Psycopg2Transaction(Transaction, driver=psycopg2):
 
-    def _at_enter(self):
-        self._current.conn.rollback()
-        self._current.conn.autocommit = False
+    def _enable_params(self):
+        conn = self._current.conn
+        if conn.info.transaction_status != TRANSACTION_STATUS_IDLE:
+            conn.rollback()
+        conn.autocommit = False
+        if level := self._params.get('level'):
+            self._old_isolation_level = conn.isolation_level
+            conn.set_session(isolation_level=level.upper())
 
-    def _at_exit(self):
-        self._current.conn.autocommit = True
+    def _restore_params(self):
+        conn = self._current.conn
+        if hasattr(self, '_old_isolation_level'):
+            conn.set_session(isolation_level=self._old_isolation_level)
+
+    def _start_savepoint(self):
+        self._savepoint_name = f'sp_{id(self)}'
+        cursor = self._current.conn.cursor()
+        try:
+            cursor.execute(f'SAVEPOINT {self._savepoint_name}')
+        finally:
+            cursor.close()
+
+    def _release_savepoint(self):
+        cursor = self._current.conn.cursor()
+        try:
+            cursor.execute(f'RELEASE SAVEPOINT {self._savepoint_name}')
+        finally:
+            cursor.close()
+
+    def _rollback_savepoint(self):
+        cursor = self._current.conn.cursor()
+        try:
+            cursor.execute(f'ROLLBACK TO SAVEPOINT {self._savepoint_name}')
+            cursor.execute(f'RELEASE SAVEPOINT {self._savepoint_name}')
+        finally:
+            cursor.close()
 
 
 class Psycopg2ConnectionValidator(ConnectionValidator, driver=psycopg2):
 
     def validate(self, conn):
         try:
-            cur = conn.cursor()
-            cur.execute('SELECT 1')
+            cursor = conn.cursor()
+            cursor.execute('SELECT 1')
+            cursor.fetchone()
+            cursor.close()
         except Exception:
             return False
         return True
@@ -34,7 +65,8 @@ class Psycopg2ConnectionValidator(ConnectionValidator, driver=psycopg2):
         status = conn.info.transaction_status
         if status == TRANSACTION_STATUS_UNKNOWN:
             return False
-        elif status != TRANSACTION_STATUS_IDLE:
+        try:
             conn.rollback()
-            return True
-        return True
+        except Exception:
+            return False
+        return self.validate(conn)
